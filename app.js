c4c35937ed5c1107cbb33a33f3058e46ff4b8203
Mahardhika Pratama <putrad578@gmail.com>
2023-03-19 20:54:58 +0700

    refactor: penggunaan async-await bersamaan dengan try-catch(untuk menampilkan error apabila terjadi kesalahan

--- app.js
+++ app.js
@@ -41,12 +41,17 @@
 //   });
 
 const loadPeople = async () => {
-  const rest = await fetch('https://swapi.dev/api/people/1');
-  const data = await rest.json();
-  console.log(data);
-  const rest2 = await fetch('https://swapi.dev/api/people/2');
-  const data2 = await rest2.json();
-  console.log(data2);
+  try {
+    const rest = await fetch('https://swapi.dev/api/people/1');
+    const data = await rest.json();
+    console.log(data);
+    const rest2 = await fetch('https://swapi.dev/api/people/2');
+    const data2 = await rest2.json();
+    console.log(data2);
+  } catch (err) {
+    //proses lainnya untuk menampilkan error
+    console.log('error', err);
+  }
 };
 
 loadPeople();
