858da5c67cb6143b531a4c762ca7581c57946770
Mahardhika Pratama <putrad578@gmail.com>
2023-03-21 13:12:00 +0700

    refactor: request dengan library axios

--- app.js
+++ app.js
@@ -40,18 +40,27 @@
 //     console.log('error', err);
 //   });
 
-const loadPeople = async () => {
-  try {
-    const rest = await fetch('https://swapi.dev/api/people/1');
-    const data = await rest.json();
-    console.log(data);
-    const rest2 = await fetch('https://swapi.dev/api/people/2');
-    const data2 = await rest2.json();
-    console.log(data2);
-  } catch (err) {
-    //proses lainnya untuk menampilkan error
-    console.log('error', err);
-  }
-};
+axios
+  .get('https://swapi.dev/api/people/1')
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-loadPeople();
+// const loadPeople = async () => {
+//   try {
+//     const rest = await fetch('https://swapi.dev/api/people/1');
+//     const data = await rest.json();
+//     console.log(data);
+//     const rest2 = await fetch('https://swapi.dev/api/people/2');
+//     const data2 = await rest2.json();
+//     console.log(data2);
+//   } catch (err) {
+//     //proses lainnya untuk menampilkan error
+//     console.log('error', err);
+//   }
+// };
+
+// loadPeople();
